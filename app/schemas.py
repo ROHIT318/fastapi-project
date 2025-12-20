6d121f7c646b3fe86329b1496aa730ea9d1fca04
Rohit Sharma <iro1hit@gmail.com>
2025-12-20 19:17:27 +0530

    Pydantic validation of model/table.

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,8 @@
-from  pydantic import BaseModel
+from  pydantic import BaseModel, Field
+from typing import Annotated
 
-class EntrySchema(BaseModel):
-    id: int
-    fname: str
-    lname: str
-    location: str
+class EntryCreate(BaseModel):
+    id: Annotated[int, Field(strict=True)]
+    fname: Annotated[str, Field(strict=True)]
+    lname: Annotated[str, Field(strict=True)]
+    dp_url: Annotated[str, Field(strict=True)]
